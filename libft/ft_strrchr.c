148cad95efe20d155d25a49929a6a6fe8278a025
Kevin rad Poquita <kpoquita@lab2r3s3.42abudhabi.ae>
2021-10-11 23:11:47 +0400

    added ft_strrchr.c

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -1,27 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kpoquita <kpoquita@42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/11 17:48:46 by kpoquita          #+#    #+#             */
-/*   Updated: 2021/10/11 17:49:48 by kpoquita         ###   ########.fr       */
+/*   Created: 2021/10/11 17:53:36 by kpoquita          #+#    #+#             */
+/*   Updated: 2021/10/11 19:36:27 by kpoquita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
 	char	*str;
+	int		i;
 
 	str = (char *) s;
-	while (*str != c)
+	i = ft_strlen(str);
+	if (c == '\0')
+		return (str + i);
+	while (i >= 0)
 	{
-		if (!*str)
-			return (NULL);
-		str++;
+		if (str[i] == (char) c)
+			return (str + i);
+		i--;
 	}
-	return (str);
+	return (NULL);
 }
